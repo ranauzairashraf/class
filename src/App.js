0d01f14f940a5d47e56c594682eaa982e1fc658e
ranauzairashraf <uzairashrafamazon05@gmail.com>
2023-01-20 18:48:06 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useMemo, useState } from "react";
+import {nav} from "./components/nav";
+const idb =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+const insertDataInIndexedDb = () => {
+  //check for support
+  if (!idb) {
+    console.log("This browser doesn't support IndexedDB");
+    return;
+  }
+}
+   const request = idb.open("test-db", 1);
+   request.onerror = function (event) {
+    console.error("An error occurred with IndexedDB");
+    console.error(event);
+  };
+   request.onupgradeneeded = function (event) {
+    console.log(event);
+    const db = request.result;
+
+    if (!db.objectStoreNames.contains("userData")) {
+      const objectStore = db.createObjectStore("userData", { keyPath: "id" });
+
+      objectStore.createIndex("age", "age", {
+        unique: false,
+      });
+    }
+  };
+  // request.onsuccess = function () {
+  //   console.log("Database opened successfully");
+
+  //   const db = request.result;
+
+  //   var tx = db.transaction("userData", "readwrite");
+  //   var userData = tx.objectStore("userData");
+
+  //   USER_DATA.forEach((item) => userData.add(item));
+
+  //   return tx.complete;
+  // };
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+
+<nav/>
+
+  </>
   );
 }
 
